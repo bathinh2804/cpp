5c7f4ba4532fd55204755f5aed68a51ac13938a6
PHẠM BÁ THỊNH <bathinh0904@gmail.com>
2023-03-02 19:39:26 +0700

    cpp

--- c++/c1/bt3.cpp
+++ c++/c1/bt3.cpp
@@ -7,6 +7,7 @@ struct DIEM
 {
 	int x;
 	int y;
+    int z;
     
 };
 
@@ -15,34 +16,38 @@ void NhapToaDo(DIEM &d)
 {
 	cout<<"Nhap x  "; cin>> d.x;
 	cout<<"Nhap y "; cin>> d.y;
+    cout<<"Nhap z  "; cin>> d.z;
 }
 
 void XuatToaDo(DIEM d)
 {
-	cout<<"("<<d.x<<","<<d.y<<")";
+	cout<<"("<<d.x<<","<<d.y<<","<<d.z<<")";
 }
 
 
 
 int main()
 {
-	DIEM x, y;
+	DIEM x, y,z;
 	
 	cout<<"Diem x:\n";
 	NhapToaDo(x);
 
 	cout<<"\nDiem y: \n";
 	NhapToaDo(y);
-    ;
+    cout<<"Diem z:\n";
+	NhapToaDo(z);
 
 	cout<<"\nToa do a la: ";
 	XuatToaDo(x);
 
 	cout<<"\nToa do  y la: ";
 	XuatToaDo(y);
-    
-	
+
+    cout<<"\nToa do  z la: ";
+	XuatToaDo(z);	
 	
 	cout<<endl;
+    return 0 ;
 }
 	
